bc717de43f53b37cd7b15d5cd65d458d2d1e4790
aSaul2006 <as200665@gmail.com>
2013-05-28 11:20:44 -0700

    Add Database

--- Space Star 5/Space Star 5/GameOverScreen.cpp
+++ Space Star 5/Space Star 5/GameOverScreen.cpp
@@ -2,6 +2,7 @@
 #include <stdio.h>
 
 
+
 GameOverScreen::GameOverScreen(void)
 {
 	print = "";
@@ -20,6 +21,15 @@ void GameOverScreen::Initialize(void)
 	D3DXCreateTextureFromFileEx(Initializer::GetInstance()->GetDevice(),
 		L"titlepic.png", 800, 600, 0, 0, D3DFMT_UNKNOWN, D3DPOOL_MANAGED,
 		D3DX_DEFAULT, D3DX_DEFAULT, NULL, NULL, NULL, &bgTex);
+	
+
+	//Connect to the Database
+	if(!db_HighScoreConnect.connect(L"highscores",L"",L""))
+	{
+			print = "Connection Failed";
+	}
+	else
+		print = "Connection Awesome";
 }
 
 void GameOverScreen::Update(GameState& gameState, float dt)
@@ -41,13 +51,14 @@ void GameOverScreen::Render(void)
 	D3DCOLOR fontColor;
 
 	//store the output in print variable
-	print = "Game Over";
+	//print = "Game Over";
 	SetRect(&rect, 350, 400, 450, 600);
 	fontColor = D3DCOLOR_RGBA(192, 192, 192, 255);
 
 	//draw text
 	Initializer::GetInstance()->GetFont()->DrawTextA(0, print.c_str(), -1, &rect,
 		DT_CENTER | DT_NOCLIP, fontColor);
+
 }
 
 void GameOverScreen::Shutdown(void)
--- Space Star 5/Space Star 5/GameOverScreen.h
+++ Space Star 5/Space Star 5/GameOverScreen.h
@@ -1,6 +1,8 @@
 #pragma once
 
 #include "Screen.h"
+#include <iostream>
+#include "Database\tiodbc.hpp"
 
 //safe release
 #define SAFE_RELEASE(x) if(x){x->Release(); x=0;}
@@ -9,6 +11,10 @@ class GameOverScreen : public CScreen
 {
 private:
 	IDirect3DTexture9* bgTex;
+
+	//Database stuff
+	tiodbc::connection db_HighScoreConnect;
+	tiodbc::statement createStmt, readStmt, updateStmt, deleteStmt;
 public:
 	GameOverScreen(void);
 	~GameOverScreen(void);
